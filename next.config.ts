f216ced44481a0aad97aefb06186485ab9a55471
Daniel Philip Johnson <daniel-philip-johnson@outlook.com>
2025-05-09 22:20:02 +0100

    feat: deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/notion-markdown-previewer",
+  assetPrefix: "/notion-markdown-previewer/",
 };
 
 export default nextConfig;
