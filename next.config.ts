06d19f7167e94fb01b17ab869423885bc42db1b1
Daniel Philip Johnson <daniel-philip-johnson@outlook.com>
2025-05-09 22:30:47 +0100

    fix: update basePath in next.config.ts to include trailing slash and add .nojekyll file for deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/notion-markdown-previewer",
+  basePath: "/notion-markdown-previewer/",
   assetPrefix: "/notion-markdown-previewer/",
 };
 
